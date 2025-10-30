1f0c9e92c902f4e3d521a0f70a94254809c0c407
Deju Adejo <ojomideju2003@gmail.com>
2025-10-30 08:45:18 +0100

    refactor: move router and authprovider to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import AuthProvider from "./context/AuthContext";
+import useAuth from "./hooks/useAuth";
 import AppRoutes from "./routes/AppRoutes";
 import SideBar from "./components/layout/SideBar";
 
 function App() {
+  const { authenticated } = useAuth();
   const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <Router>
-      <AuthProvider>
-        <div className="flex">
-          <SideBar
-            darkMode={darkMode}
-            onToggleTheme={() => setDarkMode(!darkMode)}
-          />
-          <main className="flex-1 p-8">
-            <AppRoutes />
-            <h1 className="text-2xl font-bold">Welcome to Relatus.AI Clone</h1>
-          </main>
-        </div>
-      </AuthProvider>
-    </Router>
+    <div className="flex">
+      {authenticated && (
+        <SideBar
+          darkMode={darkMode}
+          onToggleTheme={() => setDarkMode(!darkMode)}
+        />
+      )}
+      <main className="flex-1">
+        <AppRoutes />
+      </main>
+    </div>
   );
 }
 
